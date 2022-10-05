4174403b4193697f497333cffa8ce4257f1121dc
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-10-04 22:06:32 -0300

    Cidade pesquisada é buscada na API

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,3 @@
-//c0248e94dec6f66826cb62438335f605
-
 const inputCidade = document.getElementById('input-cidade');
 const botao = document.getElementById('btn-pesquisa');
 botao.addEventListener('click', pesquisar);
@@ -24,7 +22,10 @@ function pesquisar() {
     getDadosApi(cidade);
 }
 
-function getDadosApi(cidade){
+async function getDadosApi(cidade){
 
-    console.log(cidade);
+    let URLgetDados = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
+    let getDadosFetch = await fetch(URLgetDados);
+    let getDados = getDadosFetch.json();
+    console.log(getDados);
 }
